557221e8bfd7f1ed90817a53051a1ac74ae360f4
HarryPotter <taipq@saokhue.io>
2025-02-05 22:26:23 +0700

    fix: mongodb remove

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,34 +2,14 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { ScriptsModule } from './modules/scripts/scripts.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppMiddleware } from './middleware/app.middleware';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './jwt/constants';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (config: ConfigService) => ({
-        uri: config.get<string>('MONGO_URI'),
-        dbName: config.get<string>('MONGO_DB'),
-        auth: {
-          username: config.get<string>('MONGO_USER'),
-          password: config.get<string>('MONGO_PASS'),
-        },
-      }),
-      inject: [ConfigService],
-    }),
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '7200s' },
     }),
     ScheduleModule.forRoot(),
     ScriptsModule,
